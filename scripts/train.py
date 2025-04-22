3a8b5e24fb70fbddbd27e7d37a3c9979793489fd
Peerawat <peerawat2002@gmail.com>
2025-04-22 22:48:07 +0700

    yaml loader & argparser

--- scripts/train.py
+++ scripts/train.py
@@ -1,45 +1,57 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import tqdm,torch
 from Game_2048.board import Board, main_loop
 from RL_Algorithm.RL_base import BaseAlgorithm
 from RL_Algorithm.Algorithm.DQN import DQN
+import yaml
+import argparse
 
-n_episodes = 10
-target_update_interval = 1
-initial_epsilon = 1.0
-epsilon_decay = 0.998
-final_epsilon = 0.05
-learning_rate = 1e-3
-discount_factor =  0.99
-tau = 0.95
-batch_size = 8
-buffer_size = 100
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Select algorithm and experiment.')
+parser.add_argument('--algo', type=str, required=True, help='Name of the algorithm (e.g., DuelingDQN)')
+parser.add_argument('--exp', type=str, required=True, help='Experiment name (e.g., experiment_1)')
+parser.add_argument('--debug', type=bool, default=False, help='True if you want to track the training process')
+args = parser.parse_args()
+
+debug_flag = args.debug
+
+# Load the YAML file
+with open(f'params/{args.algo}.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+
+selected_config = config[args.algo][args.exp]
+print(selected_config)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 board_env = Board()
 
 #//////////////// Algorithm selection ///////////////////////////
-agent = DQN(initial_epsilon=initial_epsilon,
-            epsilon_decay=epsilon_decay,
-            final_epsilon=final_epsilon,
-            learning_rate=learning_rate,
-            discount_factor=discount_factor,
-            tau=tau,
-            batch_size=batch_size,
-            buffer_size=buffer_size,
+agent = DQN(initial_epsilon=selected_config['initial_epsilon'],
+            epsilon_decay=selected_config['epsilon_decay'],
+            final_epsilon=selected_config['final_epsilon'],
+            learning_rate=selected_config['learning_rate'],
+            discount_factor=selected_config['discount_factor'],
+            tau=selected_config['tau'],
+            batch_size=selected_config['batch_size'],
+            buffer_size=selected_config['buffer_size'],
             device=device)
 #///////////////////////////////////////////////////////////////
 
 total_scores = []
 best_tile_list = []
 
-for episode in range(n_episodes):
+for episode in range(selected_config['n_episodes']):
     board_env.reset()
     done = False
     cumulative_reward = 0
     step_count = 0
 
-    print(board_env.board)
     state = agent.encode_state(board_env.board)
     non_valid_count, valid_count = 0, 0
     while not done:
@@ -96,6 +108,6 @@ for episode in range(n_episodes):
                 print(f"50 episode running average: {average}")
             break
 
-    if episode%target_update_interval == 0:
+    if episode%selected_config['target_update_interval'] == 0:
         agent.update_target_network()
         
