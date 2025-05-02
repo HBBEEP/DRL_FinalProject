99d470e5741d02bf277550adac76210bd6cf7c59
Peerawat <peerawat2002@gmail.com>
2025-05-02 12:01:19 +0700

    Update DQNFamily & code structures

--- RL_Algorithm/DQN_Family.py
+++ RL_Algorithm/DQN_Family.py
@@ -3,30 +3,16 @@ from torch import nn
 import torch
 import torch.nn.functional as F
 from RL_Algorithm.RL_base import BaseAlgorithm
+from RL_Algorithm.Network.DQN import DQN_network
+from RL_Algorithm.Network.DoubleDQN import DoubleDQN_network
+from RL_Algorithm.Network.DuelingDQN import Dueling_DQN_network
 
-class DQN_network(nn.Module):
-    def __init__(self, device:str, hidden_dim:int = 2048):
-        self.device = device
-        super(DQN_network, self).__init__()
-        self.conv1 = nn.Conv2d(16, hidden_dim//8, (2,2))
-        self.conv2 = nn.Conv2d(hidden_dim//8, hidden_dim//4, (2,2))
-        self.dense1 = nn.Linear(hidden_dim, hidden_dim//2)
-        self.dense6 = nn.Linear(hidden_dim//2, hidden_dim//4)
-        self.dense7 = nn.Linear(hidden_dim//4, 4)
-    
-    def forward(self, x):
-        x = x.to(self.device)
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.reshape(x.size(0), -1)
-        x = F.relu(self.dense1(x))
-        x = F.relu(self.dense6(x))
-        x = self.dense7(x)
-        return x
 
-class DQN(BaseAlgorithm):
+
+class DQNFamily(BaseAlgorithm):
     def __init__(
             self,
+            algorithm:str,
             initial_epsilon:float,
             epsilon_decay:float,
             final_epsilon:float,
@@ -40,9 +26,17 @@ class DQN(BaseAlgorithm):
             device:str = 'cuda',
     ) -> None:
         self.device = device
-        policy_network = DQN_network(hidden_dim=hidden_dim,device=self.device).to(device=self.device)
-        target_network = DQN_network(hidden_dim=hidden_dim,device=self.device).to(device=self.device)
-  
+
+        if algorithm == "DQN":
+            policy_network = DQN_network(hidden_dim=hidden_dim,device=self.device).to(device=self.device)
+            target_network = DQN_network(hidden_dim=hidden_dim,device=self.device).to(device=self.device)
+        elif algorithm == "DoubleDQN":
+            policy_network = DoubleDQN_network(hidden_dim=hidden_dim,device=self.device).to(device=self.device)
+            target_network = DoubleDQN_network(hidden_dim=hidden_dim,device=self.device).to(device=self.device)
+        elif algorithm == "DuelingDQN":
+            policy_network = Dueling_DQN_network(input_dim=16, hidden_dim=hidden_dim, num_actions=4, device=self.device).to(device=self.device)
+            target_network = Dueling_DQN_network(input_dim=16, hidden_dim=hidden_dim, num_actions=4, device=self.device).to(device=self.device)
+
         policy_network.train()
         target_network.eval()
         
@@ -61,8 +55,6 @@ class DQN(BaseAlgorithm):
             soft_update = soft_update,
             device=device
         )
-
-        self.previous_weight = torch.zeros_like(self.policy_network.dense1.weight)
     
     def calculate_loss(self, batch):
         non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
